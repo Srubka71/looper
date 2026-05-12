f97a154e3e592071863d9eeb90844a7a9bde96a7
Srubka71 <vertuigames@gmail.com>
2026-05-12 23:07:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const TOTAL_SLOTS = 36;
 const beats = [
   {
     id: 1,
-    title: "Dark Bounce",
-    producer: "SPK",
+    title: "Jakiś bit nr1",
+    producer: "Nie wiem",
     bpm: 140,
     file: "assets/audio/beat1.mp3",
     image: "assets/images/beat1.jpg",
@@ -12,8 +12,8 @@ const beats = [
   },
   {
     id: 2,
-    title: "Drill Night",
-    producer: "SPK",
+    title: "Ale nuta ło ja cie ",
+    producer: "Ktoś tam",
     bpm: 145,
     file: "assets/audio/beat2.mp3",
     image: "assets/images/beat2.jpg",
@@ -21,8 +21,8 @@ const beats = [
   },
   {
     id: 3,
-    title: "Drill Night 2",
-    producer: "SPK",
+    title: "Ale nakurcza v3",
+    producer: "Somsiad mechanik",
     bpm: 145,
     file: "assets/audio/beat3.mp3",
     image: "assets/images/beat3.jpg",
@@ -31,7 +31,7 @@ const beats = [
   {
     id: 4,
     title: "Jakoś tak",
-    producer: "SPK",
+    producer: "Wojnowicza",
     bpm: 145,
     file: "assets/audio/beat4.mp3",
     image: "assets/images/beat4.jpg",
@@ -40,7 +40,7 @@ const beats = [
    {
     id: 5,
     title: "Jakoś tak3",
-    producer: "TaK",
+    producer: "Czesio",
     bpm: 12,
     file: "assets/audio/beat5.mp3",
     image: "assets/images/beat5.jpg",
@@ -49,10 +49,55 @@ const beats = [
   {
     id: 6,
     title: "Nie wiem",
-    producer: "TaK",
+    producer: "Gienio",
     bpm: 60,
     file: "assets/audio/beat6.mp3",
     image: "assets/images/beat6.jpg",
+    available: true
+  },
+   {
+    id: 7,
+    title: "Nie wiem ale 7",
+    producer: "Dalej nie wiem",
+    bpm: 110,
+    file: "assets/audio/beat7.mp3",
+    image: "assets/images/beat7.jpg",
+    available: true
+  },
+   {
+    id: 8,
+    title: "Nie wiem ale 8",
+    producer: "Wiem ale nie powiem",
+    bpm: 69,
+    file: "assets/audio/beat8.mp3",
+    image: "assets/images/beat8.jpg",
+    available: true
+  },
+   {
+    id: 9,
+    title: "Nie wiem, ale sie dowiem",
+    producer: "a jakże że nie wiem",
+    bpm: 71,
+    file: "assets/audio/beat9.mp3",
+    image: "assets/images/beat9.jpg",
+    available: true
+  },
+   {
+    id: 10,
+    title: "Zielona Dolina",
+    producer: "Jakiś Zenek",
+    bpm: 120,
+    file: "assets/audio/beat10.mp3",
+    image: "assets/images/beat10.jpg",
+    available: true
+  },
+   {
+    id: 11,
+    title: "Co to to nie tera to nie",
+    producer: "Magdalena z Kamionki",
+    bpm: 110,
+    file: "assets/audio/beat11.mp3",
+    image: "assets/images/beat11.jpg",
     available: true
   },
 ]
