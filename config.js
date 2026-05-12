b25a9b5856e294a0deaf6f1d59a5d091fffd9d8e
Srubka71 <vertuigames@gmail.com>
2026-05-12 22:13:06 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,58 @@
+const TOTAL_SLOTS = 36;
+
+const beats = [
+  {
+    id: 1,
+    title: "Dark Bounce",
+    producer: "SPK",
+    bpm: 140,
+    file: "assets/audio/beat1.mp3",
+    image: "assets/images/beat1.jpg",
+    available: true
+  },
+  {
+    id: 2,
+    title: "Drill Night",
+    producer: "SPK",
+    bpm: 145,
+    file: "assets/audio/beat2.mp3",
+    image: "assets/images/beat2.jpg",
+    available: true
+  },
+  {
+    id: 3,
+    title: "Drill Night 2",
+    producer: "SPK",
+    bpm: 145,
+    file: "assets/audio/beat3.mp3",
+    image: "assets/images/beat3.jpg",
+    available: true
+  },
+  {
+    id: 4,
+    title: "Jakoś tak",
+    producer: "SPK",
+    bpm: 145,
+    file: "assets/audio/beat4.mp3",
+    image: "assets/images/beat4.jpg",
+    available: true
+  },
+   {
+    id: 5,
+    title: "Jakoś tak3",
+    producer: "TaK",
+    bpm: 12,
+    file: "assets/audio/beat5.mp3",
+    image: "assets/images/beat5.jpg",
+    available: true
+  },
+  {
+    id: 6,
+    title: "Nie wiem",
+    producer: "TaK",
+    bpm: 60,
+    file: "assets/audio/beat6.mp3",
+    image: "assets/images/beat6.jpg",
+    available: true
+  },
+]
