b39195acdd3ee31ef680302352d45dc1ceb9385a
Srubka71 <vertuigames@gmail.com>
2026-05-13 00:04:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ if (isSafari) {
 let loopCounter = 0;
 
 // =======================
+// BROWSER DETECTION
+// =======================
+
+const isSafari =
+  /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if (isSafari) {
+  document.documentElement.classList.add("is-safari");
+}
+
+// =======================
 // UI REFERENCES
 // =======================
 
